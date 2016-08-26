30119b8825086fda7e2cdb2e18e831896efb44b9
Alex Ramsdell <alexramsdell@gmail.com>
2016-08-26 17:44:48 -0400

    renamed notes to keys

--- app/components/synth/components/keyboard/models/keys.js
+++ app/components/synth/components/keyboard/models/keys.js
@@ -4,13 +4,13 @@ steal(
 
     function(scaleMaker) {
 
-    var notes = {};
+    var keys = {};
     var noteNames = ['A','A#','B','C','C#','D','D#','E','F','F#','G','G#'];
     var levels = [0,1,2,3,4,5,6,7,8];
 
     noteNames.map(note => {
 
-        notes[note] = levels.map(level => {
+        keys[note] = levels.map(level => {
             return {
                 name: note + level,
                 hertz: scaleMaker.getNote(note + level) 
@@ -20,6 +20,6 @@ steal(
 
     });
 
-    return notes;
+    return keys;
 
 });
